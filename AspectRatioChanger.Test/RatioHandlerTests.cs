90e1c7a3d439b813646e5f9b35a60072bebb2bd9
Oscar Bremer <oscar.bremer@agria.se>
2024-04-17 10:34:05 +0200

    Added publish compressed

--- AspectRatioChanger.Test/RatioHandlerTests.cs
+++ AspectRatioChanger.Test/RatioHandlerTests.cs
@@ -70,4 +70,85 @@ public class RatioHandlerTests
         Assert.Null(dockedModes[0].dock_aspect_w);
         Assert.Null(dockedModes[0].dock_aspect_h);
     }
+
+    [Fact]
+    public void GetScaledPercentage_should_calculate_correct_percentage()
+    {
+        // Arrange
+        var testData = new VideoRoot
+        {
+            aspect_w = 140,
+            aspect_h = 90,
+            dock_aspect_w = 154, 
+            dock_aspect_h = 90
+        };
+
+        // Act
+        var scaledPercentage = _sut.GetScaledPercentage(testData);
+
+        Assert.Equal(110, scaledPercentage);
+    }
+
+    [Fact]
+    public void Strange_GetScaledPercentage_should_calculate_correct_percentage()
+    {
+        // Arrange
+        var testData = new VideoRoot
+        {
+            aspect_w = 50,
+            aspect_h = 30,
+            dock_aspect_w = 55,
+            dock_aspect_h = 30
+        };
+
+        // Act
+        var scaledPercentage = _sut.GetScaledPercentage(testData);
+
+        Assert.Equal(110, scaledPercentage);
+    }
+
+    [Theory]
+    [InlineData(16, 10)]
+    [InlineData(4, 3)]
+    [InlineData(8, 7)]
+    [InlineData(5, 4)]
+    public void Widescreen_aspect_ratio_should_be_stretched_correctly_unless_overstretch(int width, int height)
+    {
+        // Arrange
+        var testData = new List<VideoRoot> { new() { aspect_w = width, aspect_h = height } };
+
+
+        for (int i = 101; i < 150; i++)
+        {
+            // Act
+            double precentageDecimal = (double)i/ 100;
+            var dockedModes = _sut.AddDockedModes(testData, precentageDecimal);
+            
+            // Assert
+            var scaledPercentage = _sut.GetScaledPercentage(dockedModes[0]);
+            Assert.Equal(i, scaledPercentage);
+        }
+       
+    }
+
+
+    [Theory]
+    [InlineData(5, 3)]
+    [InlineData(21, 10)]
+    [InlineData(16, 7)]
+    [InlineData(2, 1)]
+    public void Widescreen_aspect_ratio_should_be_not_overstretch(int width, int height)
+    {
+        // Arrange
+        var testData = new List<VideoRoot> { new() { aspect_w = width, aspect_h = height } };
+
+        // Act
+        var dockedModes = _sut.AddDockedModes(testData, 1.1);
+
+        // Assert
+        var scaledPercentage = _sut.GetScaledPercentage(dockedModes[0]);
+
+        Assert.NotEqual(110, scaledPercentage);
+        Assert.True(scaledPercentage < 110);
+    }
 }
